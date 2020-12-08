4fd14704cc63535d4349a9f9222c54a5b9e407a0
unknown <shellymeter@gmail.com>
2020-12-07 18:31:16 -0800

    Book chapter 1 exe

---
+++ my_code/Chapter_1/chapter_1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 18:17:18 2020
+
+@author: Shelly Meter
+"""
+
+""" Comment for exe
+print("Hello, world!)
+
+Get error:
+SyntaxError: EOL while scanning string literal
+"""
+
+"""
+print(Hello, world!)
+
+Get error:
+SyntaxError: invalid syntax
+"""
+
+#this is my comment
+
+"""Exercise 2"""
+print("Seconds in 42 min and 42 sec:", (42*60) + 42)
---
+++ my_code/my_first_program.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 18:11:22 2020
+
+@author: Shelly Meter
+"""
+
+print("Hello World")
+print("End of program")
